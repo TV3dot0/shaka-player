04d122fa59ff0792a715cdca8921055d062227cd
lbelyaev <lbelyaev@tv3dot0.com>
2019-11-26 15:02:16 -0800

    changed default version

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-git checkout experimental
+if [ "$1" != "" ]; then
+    echo "Checkout branch: " "$1"
+    BRANCH="$1"
+else
+    echo "Checkout branch: v2.5.x"
+    BRANCH="v2.5.x"
+
+fi
+
+git checkout ${BRANCH}
+
 git pull
 
 python build/all.py --force --fix
